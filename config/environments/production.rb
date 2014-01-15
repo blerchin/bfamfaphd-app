edb139fe522c594f33b9e293f70bd9673036d072
Ben Lerchin <ben@lerchin.com>
2014-01-15 15:01:20 -0800

    X-Frame and margin for iframe

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -38,7 +38,7 @@ BfamfaphdApp::Application.configure do
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
-	config.action_dispatch.default_headers['X-Frame-Options'] = "ALLOW-FROM http://bfamfaphd.com http://localhost"
+	config.action_dispatch.default_headers['X-Frame-Options'] = "ALLOW-FROM http://bfamfaphd.com
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
